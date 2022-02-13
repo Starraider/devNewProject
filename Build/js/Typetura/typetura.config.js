eabebb0e53db76ca1cc2f37013e030c1dbaab491
Sven Kalbhenn <sven@skom.de>
2022-02-13 16:37:45 +0530

    [FEATURE]: Add Typetura.js

---
+++ Build/js/Typetura/typetura.config.js
@@ -0,0 +1,13 @@
+window.typetura = {
+  selectors: [
+    ':root',
+    '.typetura',
+    'main',
+    'article',
+    'section',
+    'aside',
+    'contentcontainer-column',
+  ],
+  base: 16,
+  scale: 2,
+}
---
+++ Build/js/head.js
@@ -0,0 +1,16 @@
+// Typetura.js
+//import * as Typeturaconfig from './Typetura/typetura.config'
+
+window.typetura = {
+  selectors: [
+    ':root',
+    '.typetura',
+    'main',
+    'article',
+    'section',
+    'aside',
+    'contentcontainer-column',
+  ],
+}
+
+import './Typetura/typetura'
