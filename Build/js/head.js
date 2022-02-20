d75e0833546acdb0fce87751d6f014d7ef487df1
Sven Kalbhenn <sven@skom.de>
2022-02-20 15:02:13 +0530

    [TASK]: Config Typetura

--- Build/js/head.js
+++ Build/js/head.js
@@ -1,16 +1,3 @@
 // Typetura.js
 //import * as Typeturaconfig from './Typetura/typetura.config'
-
-window.typetura = {
-  selectors: [
-    ':root',
-    '.typetura',
-    'main',
-    'article',
-    'section',
-    'aside',
-    'contentcontainer-column',
-  ],
-}
-
-import './Typetura/typetura'
+//import './Typetura/typetura'
