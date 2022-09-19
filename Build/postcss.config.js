3e4d1f41d375a0220b1a397ba4681e281039d798
Sven Kalbhenn <sven@skom.de>
2022-09-19 17:34:01 +0545

    [FEATURE]: Add postcss plugins

--- Build/postcss.config.js
+++ Build/postcss.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    plugins: {
-        // include whatever plugins you want
-        // but make sure you install these via yarn or npm!
+  plugins: {
+    // include whatever plugins you want
+    // but make sure you install these via yarn or npm!
 
-        // add browserslist config to package.json (see below)
-        'postcss-import': {},
-        'postcss-discard-comments': {},
-        autoprefixer: {},
-        cssnano: {
-            preset: 'default',
-        },
+    // add browserslist config to package.json (see below)
+    'postcss-import': {},
+    'postcss-preset-env': {
+      stage: 1,
     },
+    'postcss-write-svg': {},
+    'postcss-discard-comments': {},
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
+  },
 }
