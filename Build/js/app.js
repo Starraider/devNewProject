17202948be504c6611bffa16eec201d866adbf0f
Sven Kalbhenn <sven@skom.de>
2022-09-19 17:34:25 +0545

    [FEATURE]: Add postcss plugins

--- Build/js/app.js
+++ Build/js/app.js
@@ -5,7 +5,7 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+console.log('Starting build...')
 /*
  * any CSS you import will output into a single css file (main.css in this case)
  */
